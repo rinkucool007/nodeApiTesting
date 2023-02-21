ffeef6261fc8c63be5ee8b207d793daaa760a57d
akash <pratap_the_knight@yahoo.com>
2023-02-21 07:59:31 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+npx mocha --timeout 10000 .\chai-http-js.js
+
+
+npx mocha --timeout 10000 .\apiTesting.js
+
+
+
+npm install supertest chai csv-parser mocha mochawesome mochawesome-merge mochawesome-report-generator --save-dev
+
+npx mocha .\apiTesting.js --reporter mochawesome
---
+++ apiTestingReport.js
@@ -0,0 +1,62 @@
+const supertest = require('supertest');
+const chai = require('chai');
+const csv = require('csv-parser');
+const fs = require('fs');
+
+const mocha = require('mocha');
+const { merge } = require('mochawesome-merge');
+const reportGenerator = require('mochawesome-report-generator');
+
+const expect = chai.expect;
+const apiUrl = 'https://reqres.in'; // replace with your live API URL
+
+describe('POST /api/users', () => {
+  it('should create a new user with valid data', (done) => {
+    fs.createReadStream('testData.csv')
+      .pipe(csv())
+      .on('data', (data) => {
+        supertest(apiUrl)
+          .post('/api/users')
+          .send({
+            name: data.name,
+            job: data.job
+          })
+          .expect(201)
+          .end((err, res) => {
+            if (err) return done(err);
+            // assertions for successful response
+            expect(res.body).to.have.property('id');
+            expect(res.body.name).to.equal(data.name);
+            expect(res.body.job).to.equal(data.job);
+          });
+      })
+      .on('end', () => {
+        done();
+      });
+  });
+});
+
+describe('GET /api/users', () => {
+  it('should return all users', (done) => {
+    supertest(apiUrl)
+      .get('/api/users')
+      .end((err, res) => {
+        if (err) return done(err);
+        // assertions for successful response
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('array').that.has.lengthOf.at.least(2);
+        done();
+      });
+  });
+});
+
+const options = {
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: './reports',
+    reportFilename: 'test-report',
+    reportTitle: 'Test Report'
+  }
+};
+
+mocha.setup(options);
